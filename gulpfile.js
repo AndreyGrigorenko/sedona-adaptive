0d098032958ad396d727a586b6c63c3f523a1526
AndreyGrigorenko <grigorenko584@gmail.com>
2018-02-21 21:37:43 +0200

    Усовершенствовал сборку

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,34 @@ var plumber = require('gulp-plumber');
 var run = require('run-sequence');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 var browserSync = require('browser-sync').create();
+
+gulp.task('dev', function(fn) {
+  run(
+  	'clean',
+ 	'pug',
+ 	'sass',
+ 	'scripts',
+ 	'copy',
+    'server',
+    'watch',   
+    fn
+  );
+});
+
+gulp.task('production', function(fn) {
+  run(
+  	'clean',
+ 	'pug',
+ 	'sass',
+ 	'scripts',
+ 	'copy',       
+    fn
+  );
+});
 
 gulp.task('pug', function() {
 	gulp.src('src/pug/pages/*.pug')
@@ -27,6 +54,28 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('dist/css/'));		
 });
 
+gulp.task('scripts', function() {
+	gulp.src('src/js/**/*.js')
+		.pipe(jshint())
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src([
+    'src/fonts/**/*.{woff,woff2}',
+    'src/img/**'    
+  ], {
+    base: 'src/'
+  })
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+	return gulp.src('dist/')
+		.pipe(clean());
+})
+
 gulp.task('server', function() {
 	browserSync.init({
 		open: false,		   			
@@ -39,15 +88,7 @@ gulp.task('server', function() {
 gulp.task('watch', function() {
 	gulp.watch('src/**/*.pug', ['pug']);
 	gulp.watch('src/**/*.scss', ['sass']);
+	gulp.watch('src/js/**/*.js', ['scripts']);
+	gulp.watch(['./src/img/**/*.*', './src/fonts/**/*.*'], ['assets']);
 	gulp.watch('src/**/*.*').on('change', browserSync.reload);	
-});
-
-
-gulp.task('default', function(fn) {
-  run(
- 	'pug',
-    'server',
-    'watch',   
-    fn
-  );
 });
