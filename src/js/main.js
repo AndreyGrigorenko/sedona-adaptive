d7e6c5965128f3dfa0c5f2142c813c5455e7e095
AndreyGrigorenko <grigorenko584@gmail.com>
2018-02-28 18:38:07 +0200

    Сделал js для меню

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+;(function () {
+	var menu = document.querySelector('.main-menu');
+	var menuClose = document.querySelector('.main-menu__close');
+	var menuOpen = document.querySelector('.main-header__burger');
+
+	menu.classList.add('hide');
+	menuOpen.addEventListener('click', function(e) {
+		e.preventDefault();
+		menu.classList.remove('hide');		
+		menu.classList.add('show');		
+	});
+
+	menuClose.addEventListener('click', function(e) {
+		e.preventDefault();
+		menu.classList.remove('show');
+		menu.classList.add('hide');		
+	});
+})();
